f86c21deeb60a2abe4ce9347bb175877496446ba
QuienDuong <dttq8340@gmail.com>
2023-09-22 09:57:02 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require ("cors");
-const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
+const app = express();
+app.use("/api/contacts", contactsRouter);
 app.use(express.json());
 app.get("/", (req, res) => {
     res.json({
